cec57b1d4af32f088cef1efac95b11769ade1575
Tonny <84282878+UzCoderOff@users.noreply.github.com>
2024-09-25 19:00:42 +0500

    f

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: [
-        /^node:.*/,
-      ]
+      external: ['react-dom/client']  // Externalize react-dom/client to avoid build issues
     }
   }
 })
